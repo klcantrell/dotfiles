7cd3b39f2d76f7c8d0ddb6d49432b006d66ef5a2
Kalalau Cantrell <cantrellkalalau@gmail.com>
2026-02-28 21:10:07 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -29,11 +29,15 @@ keymap({"n", "v"}, "<leader>f", "<cmd>lua require('vscode').action('editor.actio
 keymap({"n", "v"}, "<leader>e", "<cmd>lua require('vscode').action('editor.action.marker.nextInFiles')<CR>")  
 keymap({"n", "v"}, "<leader>w", "<cmd>lua require('vscode').action('editor.action.marker.prevInFiles')<CR>")  
 keymap({"n", "v"}, "<leader>a", "<cmd>lua require('vscode').action('editor.action.quickFix')<CR>")  
+keymap({"n", "v"}, "<leader>E", "<cmd>lua require('vscode').action('workbench.files.action.focusFilesExplorer')<CR>")  
+keymap({"n", "v"}, "<leader>G", "<cmd>lua require('vscode').action('workbench.scm.focus')<CR>")  
+keymap({"n", "v"}, "<leader>R", "<cmd>lua require('vscode').action('workbench.view.extension.references-view')<CR>")  
 keymap({"n", "v"}, "gh", "<cmd>lua require('vscode').action('editor.action.showHover')<CR>")  
 keymap({"n", "v"}, "gr", "<cmd>lua require('vscode').action('editor.action.goToReferences')<CR>")  
 keymap({"n", "v"}, "gR", "<cmd>lua require('vscode').action('references-view.findReferences')<CR>")  
 keymap({"n", "v"}, "gj", "<cmd>lua require('vscode').action('workbench.action.joinAllGroups')<CR>")  
 keymap({"n", "v"}, "gX", "<cmd>lua require('vscode').action('workbench.action.closeAllEditors')<CR>")  
+keymap({"n", "v"}, "gE", "<cmd>lua require('vscode').action('workbench.files.action.showActiveFileInExplorer')<CR>")  
 keymap({"n", "v"}, "<C-l>", "<cmd>lua require('vscode').action('workbench.action.focusRightGroup')<CR>")  
 keymap({"n", "v"}, "<C-h>", "<cmd>lua require('vscode').action('workbench.action.focusLeftGroup')<CR>")  
 keymap({"n", "v"}, "<C-j>", "<cmd>lua require('vscode').action('workbench.action.focusBelowGroup')<CR>")
@@ -60,6 +64,67 @@ require('lazy').setup({
   "tpope/vim-repeat",
   "nvim-treesitter/nvim-treesitter",
   "nvim-treesitter/nvim-treesitter-textobjects",
+  {
+    "jake-stewart/multicursor.nvim",
+    branch = "1.0",
+    config = function()
+        local mc = require("multicursor-nvim")
+        mc.setup()
+
+        -- Add or skip cursor above/below the main cursor.
+        -- keymap({"n", "x"}, "<up>", function() mc.lineAddCursor(-1) end)
+        -- keymap({"n", "x"}, "<down>", function() mc.lineAddCursor(1) end)
+        -- keymap({"n", "x"}, "<leader><up>", function() mc.lineSkipCursor(-1) end)
+        -- keymap({"n", "x"}, "<leader><down>", function() mc.lineSkipCursor(1) end)
+
+        -- Add or skip adding a new cursor by matching word/selection
+        keymap({"n", "x"}, "<leader>n", function() mc.matchAddCursor(1) end)
+        keymap({"n", "x"}, "<leader>m", function() mc.matchSkipCursor(1) end)
+        keymap({"n", "x"}, "<leader>N", function() mc.matchAddCursor(-1) end)
+        keymap({"n", "x"}, "<leader>M", function() mc.matchSkipCursor(-1) end)
+        -- Add a cursor for all matches of cursor word/selection in the document.
+        keymap({"n", "x"}, "<leader>t", mc.matchAllAddCursors)
+
+        -- Add and remove cursors with control + left click.
+        -- keymap("n", "<c-leftmouse>", mc.handleMouse)
+        -- keymap("n", "<c-leftdrag>", mc.handleMouseDrag)
+        -- keymap("n", "<c-leftrelease>", mc.handleMouseRelease)
+
+        -- Disable and enable cursors.
+        keymap({"n", "x"}, "<leader>c", mc.toggleCursor)
+
+        -- Mappings defined in a keymap layer only apply when there are
+        -- multiple cursors. This lets you have overlapping mappings.
+        mc.addKeymapLayer(function(layerSet)
+
+            -- Select a different cursor as the main one.
+            -- layerSet({"n", "x"}, "<left>", mc.prevCursor)
+            -- layerSet({"n", "x"}, "<right>", mc.nextCursor)
+
+            -- Delete the main cursor.
+            -- layerSet({"n", "x"}, "<leader>x", mc.deleteCursor)
+
+            -- Enable and clear cursors using escape.
+            layerSet("n", "<esc>", function()
+                if not mc.cursorsEnabled() then
+                    mc.enableCursors()
+                else
+                    mc.clearCursors()
+                end
+            end)
+        end)
+
+        -- Customize how cursors look.
+        -- local hl = vim.api.nvim_set_hl
+        -- hl(0, "MultiCursorCursor", { link = "Cursor" })
+        -- hl(0, "MultiCursorVisual", { link = "Visual" })
+        -- hl(0, "MultiCursorSign", { link = "SignColumn"})
+        -- hl(0, "MultiCursorMatchPreview", { link = "Search" })
+        -- hl(0, "MultiCursorDisabledCursor", { link = "Visual" })
+        -- hl(0, "MultiCursorDisabledVisual", { link = "Visual" })
+        -- hl(0, "MultiCursorDisabledSign", { link = "SignColumn"})
+    end
+  }
 }, {})
 
 -- [[ Highlight on yank ]]
